dd562542a5315b16c18cdd130064a690c26b5402
jyotsnatheresz-blip <jyotsnatheresz@gmail.com>
2026-05-30 04:12:31 +0530

    Update ViewBlog.jsx

--- frontend/src/pages/ViewBlog.jsx
+++ frontend/src/pages/ViewBlog.jsx
@@ -3,11 +3,11 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function ViewBlog() {
-  const { id } = useParams(); // URL-ൽ നിന്ന് blog-ന്റെ ID എടുക്കും
+  const { id } = useParams();
   const [blog, setBlog] = useState(null);
 
   useEffect(() => {
-    axios.get(`http://localhost:5000/api/blogs/${id}`)
+    axios.get(`${process.env.REACT_APP_API_URL}/api/blogs/${id}`)
      .then(res => setBlog(res.data))
      .catch(err => console.log("Blog kittiyilla:", err));
   }, [id]);
@@ -19,7 +19,7 @@ function ViewBlog() {
       <h1>{blog.title}</h1>
       <h3>Category: {blog.category}</h3>
       {blog.image && <img 
-        src={`http://localhost:5000/uploads/${blog.image}`} 
+        src={`${process.env.REACT_APP_API_URL}/uploads/${blog.image}`} 
         alt={blog.title} 
         style={{ width: '100%', maxHeight: '400px', objectFit: 'cover' }} 
       />}
