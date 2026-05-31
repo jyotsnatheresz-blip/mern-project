4b0de2a6f1255bca4ef1cd0a1f3563e6796193b6
jyotsnatheresz-blip <jyotsnatheresz@gmail.com>
2026-05-31 23:20:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const blogRoutes = require('./routes/blog');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND_URL}));
 app.use(express.json());
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 // Routes
